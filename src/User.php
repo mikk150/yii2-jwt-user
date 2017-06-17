c101bf06a3145e467125e013ceffde4f76563ec3
Mikk Tendermann <mikk150@gmail.com>
2017-06-17 16:25:31 +0300

    fix some namespace issues

--- src/User.php
+++ src/User.php
@@ -4,17 +4,18 @@ namespace mikk150\jwt;
 
 use yii\base\Component;
 use yii\base\InvalidConfigException;
+use Yii;
 
 /**
 *
 */
-class User extends yii\web\User
+class User extends \yii\web\User
 {
     public function init()
     {
         parent::init();
 
-        if ((Yii::$app->response instanceof Response)) {
+        if (!(Yii::$app->response instanceof Response)) {
             throw new InvalidConfigException('application response object must be ' . Response::className());
         }
     }
